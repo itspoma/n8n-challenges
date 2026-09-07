4fc0bb6cac333d035b9dec7fdfa89a564d784988
Roman Rodomansky <itspoma@gmail.com>
2026-09-07 23:53:27 +0200

    Fix client hydration on 127.0.0.1

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const siteBasePath = process.env.SITE_BASE_PATH ?? "";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
   reactStrictMode: true,
   output: "export",
   basePath: siteBasePath,
